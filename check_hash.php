154b8e503550976f5ad6f92fe4d8b378df14e346
Vinhhungyn <lamvinhhung2004@gmail.com>
2026-07-04 08:55:15 +0700

    xoa file

--- check_hash.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-$c = new mysqli('mysql-patched', 'root', 'root123', 'company_db_patched', 3306);
-$r = $c->query('SELECT username, password FROM employees');
-while ($row = $r->fetch_assoc()) {
-    $ok = password_verify('admin123', $row['password']) ? 'OK' : 'FAIL';
-    echo $row['username'] . ' => ' . $ok . ' | hash: ' . $row['password'] . PHP_EOL;
-}
